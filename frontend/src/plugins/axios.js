a8e5fd47e4bf6e06867c016d2c5b89c0c6d673da
Tersawy <mohamedtersawy96@gmail.com>
2022-04-18 19:10:19 +0200

    Fix refresh page after login

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,20 +1,36 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-	baseURL: process.env.VUE_APP_BASE_URL,
-	headers: {
-		"Content-Type": "application/json",
-		Authorization: "Bearer " + sessionStorage.getItem("token")
-	}
-});
+import jsCookie from "js-cookie";
 
-axiosInstance.interceptors.response.use(
-	function (response) {
-		return { ...response.data, status: response.status };
-	},
-	function (error) {
-		return Promise.reject({ ...error.response.data, status: error.response.status });
-	}
-);
+function axiosHandler() {
+	const axiosInstance = axios.create({
+		baseURL: process.env.VUE_APP_BASE_URL,
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: "Bearer " + jsCookie.get("token")
+		}
+	});
 
-export default axiosInstance;
+	axiosInstance.interceptors.response.use(
+		function (response) {
+			return { ...response.data, status: response.status };
+		},
+		function (error) {
+			return Promise.reject({ ...error.response.data, status: error.response.status });
+		}
+	);
+
+	return axiosInstance;
+}
+
+const AxiosHandler = new Object();
+
+AxiosHandler.get =  (...args) => axiosHandler().get(...args);
+
+AxiosHandler.post =  (...args) => axiosHandler().post(...args);
+
+AxiosHandler.put =  (...args) => axiosHandler().put(...args);
+
+AxiosHandler.delete =  (...args) => axiosHandler().delete(...args);
+
+export default AxiosHandler;
