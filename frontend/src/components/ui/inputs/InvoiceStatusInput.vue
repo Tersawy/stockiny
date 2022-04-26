91ebee6d09819fc61bb45c18a6a2f7ff40295cd6
Tersawy <mohamedtersawy96@gmail.com>
2022-04-27 01:04:26 +0200

    Remove unused prop from invoice status input component

--- frontend/src/components/ui/inputs/InvoiceStatusInput.vue
+++ frontend/src/components/ui/inputs/InvoiceStatusInput.vue
@@ -3,67 +3,67 @@
 </template>
 
 <script>
-	const DefaultSelect = () => import("@/components/ui/DefaultSelect");
+const DefaultSelect = () => import("@/components/ui/DefaultSelect");
 
-	export default {
-		components: { DefaultSelect },
+export default {
+	components: { DefaultSelect },
 
-		props: ["vuelidate", "namespace", "invoiceName", "choiseEffected"],
+	props: ["vuelidate", "namespace", "invoiceName"],
 
-		async mounted() {
-			let { options } = await this.getOptions();
+	async mounted() {
+		let { options } = await this.getOptions();
 
-			let effectedOption = options.find((option) => option.effected);
+		let effectedOption = options.find((option) => option.effected);
 
-			if (effectedOption) {
-				this.$emit("mounted", effectedOption, options);
-			}
-
-			setTimeout(() => {
-				if (this.selectInput) {
-					let that = this;
-					this.selectInput.style.borderColor = that.selectedOption.color;
-
-					this.selectInput.onfocus = function () {
-						this.style.boxShadow = `0 0 0 0.06rem ${that.selectedOption.color}40`;
-					};
-
-					this.selectInput.onblur = function () {
-						this.style.boxShadow = "none";
-					};
-				}
-			});
-		},
-
-		watch: {
-			"vuelidate.status.$model": {
-				handler() {
-					if (this.selectInput) {
-						this.selectInput.style.borderColor = this.selectedOption.color;
-					}
-				},
-				deep: true
-			}
-		},
-
-		computed: {
-			options() {
-				return this.$store.state.Invoices.statuses[this.invoiceName];
-			},
-
-			selectedOption() {
-				return this.options.find((option) => this.vuelidate.status.$model == option._id) || {};
-			},
-
-			selectInput() {
-				return this.$refs.selectInput?.$refs?.customSelect?.$refs?.input;
-			}
-		},
-
-		methods: {
-			getOptions() {
-				return this.$store.dispatch("Invoices/getStatusOptions", this.invoiceName);
-			}
+		if (effectedOption) {
+			this.$emit("mounted", effectedOption, options);
 		}
-	};
+
+		setTimeout(() => {
+			if (this.selectInput) {
+				let that = this;
+				this.selectInput.style.borderColor = that.selectedOption.color;
+
+				this.selectInput.onfocus = function () {
+					this.style.boxShadow = `0 0 0 0.06rem ${that.selectedOption.color}40`;
+				};
+
+				this.selectInput.onblur = function () {
+					this.style.boxShadow = "none";
+				};
+			}
+		});
+	},
+
+	watch: {
+		"vuelidate.status.$model": {
+			handler() {
+				if (this.selectInput) {
+					this.selectInput.style.borderColor = this.selectedOption.color;
+				}
+			},
+			deep: true
+		}
+	},
+
+	computed: {
+		options() {
+			return this.$store.state.Invoices.statuses[this.invoiceName];
+		},
+
+		selectedOption() {
+			return this.options.find((option) => this.vuelidate.status.$model == option._id) || {};
+		},
+
+		selectInput() {
+			return this.$refs.selectInput?.$refs?.customSelect?.$refs?.input;
+		}
+	},
+
+	methods: {
+		getOptions() {
+			return this.$store.dispatch("Invoices/getStatusOptions", this.invoiceName);
+		}
+	}
+};
 </script>
