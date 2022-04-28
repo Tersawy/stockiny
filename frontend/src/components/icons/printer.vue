87cb52f1d29c1768563e9c5d8ef2b32c0a328d82
Tersawy <mohamedtersawy96@gmail.com>
2022-04-28 03:55:35 +0200

    Added scale property to printer icon

--- frontend/src/components/icons/printer.vue
+++ frontend/src/components/icons/printer.vue
@@ -8,6 +8,7 @@
 		v-on="$listeners"
 		xmlns="http://www.w3.org/2000/svg"
 		style="display: inline-block; overflow: visible; vertical-align: -0.15rem; outline: none; cursor: pointer"
+		:style="{ transform: `scale(${scale})` }"
 	>
 		<g>
 			<path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"></path>
@@ -33,7 +34,9 @@ export default {
 		color: {
 			type: String,
 			default: "currentColor"
-		}
+		},
+
+		scale: { type: [Number, String], default: 1 }
 	}
 };
 </script>
