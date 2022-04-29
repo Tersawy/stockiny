ed8cb3edb669e842fcbb8afe1c74d3bac2aad132
Tersawy <mohamedtersawy96@gmail.com>
2022-04-29 20:18:13 +0200

    Fix overflow for purchases table and added item index in print screen

--- frontend/src/views/Purchases/index.vue
+++ frontend/src/views/Purchases/index.vue
@@ -7,98 +7,104 @@
 			@btnExcelClicked="btnExcelClicked"
 			@btnPdfClicked="btnPdfClicked"
 		/>
-		<b-table
-			show-empty
-			stacked="lg"
-			hover
-			sort-icon-left
-			:busy="tableIsBusy"
-			:items="items"
-			:fields="fields"
-			:current-page="1"
-			:per-page="perPage"
-			:sort-by.sync="sortBy"
-			:sort-desc.sync="sortDesc"
-			@context-changed="contextChanged"
-			:filter="search"
-			:filter-function="() => items"
-			class="mb-0 text-nowrap"
-		>
-			<template #cell(actions)="row">
-				<InvoiceActions :invoice="row.item" :namespace="namespace" invoiceName="Purchase" />
-			</template>
+		<div style="overflow-x: auto" class="border-right border-left">
+			<b-table
+				show-empty
+				stacked="md"
+				hover
+				sort-icon-left
+				:busy="tableIsBusy"
+				:items="items"
+				:fields="fields"
+				:current-page="1"
+				:per-page="perPage"
+				:sort-by.sync="sortBy"
+				:sort-desc.sync="sortDesc"
+				@context-changed="contextChanged"
+				:filter="search"
+				:filter-function="() => items"
+				class="mb-0 text-nowrap"
+			>
+				<template #cell(actions)="row">
+					<InvoiceActions :invoice="row.item" :namespace="namespace" invoiceName="Purchase" />
+				</template>
 
-			<template #cell(date)="row">
-				<DateStr :date="row.value" />
-			</template>
+				<template #cell(index)="row">
+					{{ page == 1 ? row.index + 1 : (page - 1) * perPage + row.index + 1 }}
+				</template>
 
-			<template #cell(reference)="row">
-				<router-link :to="{ name: 'Purchase', params: { id: row.item._id } }" class="d-print-none">
-					{{ row.value }}
-				</router-link>
-				<span class="d-none d-print-block">{{ row.value }}</span>
-			</template>
+				<template #cell(date)="row">
+					<DateStr :date="row.value" />
+				</template>
 
-			<template #cell(supplier)="row">
-				<span> {{ row.value | relation }} </span>
-			</template>
+				<template #cell(reference)="row">
+					<router-link :to="{ name: 'Purchase', params: { id: row.item._id } }" class="d-print-none">
+						{{ row.value }}
+					</router-link>
+					<span class="d-none d-print-block">{{ row.value }}</span>
+				</template>
 
-			<template #cell(warehouse)="row">
-				<span> {{ row.value | relation }} </span>
-			</template>
+				<template #cell(supplier)="row">
+					<span> {{ row.value | relation }} </span>
+				</template>
 
-			<template #cell(total)="row">
-				<span class="text-primary font-weight-500">$ {{ row.value | floating }} </span>
-			</template>
+				<template #cell(warehouse)="row">
+					<span> {{ row.value | relation }} </span>
+				</template>
 
-			<template #cell(paid)="row">
-				<span>$ {{ row.value | floating }} </span>
-			</template>
+				<template #cell(total)="row">
+					<span class="text-primary font-weight-500">$ {{ row.value | floating }} </span>
+				</template>
 
-			<template #cell(due)="{ item }">
-				<span>$ {{ due(item) | floating }} </span>
-			</template>
+				<template #cell(paid)="row">
+					<span>$ {{ row.value | floating }} </span>
+				</template>
 
-			<template #cell(paymentStatus)="row">
-				<span v-payment-status="row.value"> </span>
-			</template>
+				<template #cell(due)="{ item }">
+					<span>$ {{ due(item) | floating }} </span>
+				</template>
 
-			<template #cell(status)="{ item }">
-				<div class="w-100 d-flex justify-content-lg-between align-items-center">
-					<InvoiceStatus :status="item.status" />
+				<template #cell(paymentStatus)="row">
+					<span v-payment-status="row.value"> </span>
+				</template>
 
-					<b-dropdown
-						right
-						variant="outline-danger"
-						size="sm"
-						toggle-class="text-decoration-none px-1 ml-4 ml-lg-2"
-						:toggle-attrs="{ style: 'padding-top:2px;padding-bottom:2px;' }"
-						no-caret
-						menu-class="py-0 shadow-sm"
-						class="d-print-none"
-					>
-						<template #button-content>
-							<ChangeIcon scale="1" v-b-tooltip title="Change Status" />
-						</template>
-						<b-overlay :show="item.status.loading" spinner-variant="secondary" spinner-small>
-							<div class="d-flex justify-content-between px-3 py-2 border-bottom" v-for="statusOption in statuses" :key="statusOption._id">
-								<div class="d-flex align-items-center">
-									<InvoiceStatus :status="statusOption" />
-									<CheckCircleOutlineIcon v-if="statusOption.effected" scale="0.7" class="ml-1" />
+				<template #cell(status)="{ item }">
+					<div class="w-100 d-flex justify-content-lg-between align-items-center">
+						<InvoiceStatus :status="item.status" />
+
+						<b-dropdown
+							right
+							variant="outline-danger"
+							size="sm"
+							toggle-class="text-decoration-none px-1 ml-4 ml-lg-2"
+							:toggle-attrs="{ style: 'padding-top:2px;padding-bottom:2px;' }"
+							no-caret
+							menu-class="py-0 shadow-sm"
+							class="d-print-none"
+						>
+							<template #button-content>
+								<ChangeIcon scale="1" v-b-tooltip title="Change Status" />
+							</template>
+							<b-overlay :show="item.status.loading" spinner-variant="secondary" spinner-small>
+								<div class="d-flex justify-content-between px-3 py-2 border-bottom" v-for="statusOption in statuses" :key="statusOption._id">
+									<div class="d-flex align-items-center">
+										<InvoiceStatus :status="statusOption" />
+										<CheckCircleOutlineIcon v-if="statusOption.effected" scale="0.7" class="ml-1" />
+									</div>
+									<b-form-checkbox
+										switch
+										class="ml-4"
+										:disabled="item.status._id == statusOption._id"
+										v-model="status(item.status, statusOption).effected"
+										@change="changeStatus(item, statusOption)"
+									></b-form-checkbox>
 								</div>
-								<b-form-checkbox
-									switch
-									class="ml-4"
-									:disabled="item.status._id == statusOption._id"
-									v-model="status(item.status, statusOption).effected"
-									@change="changeStatus(item, statusOption)"
-								></b-form-checkbox>
-							</div>
-						</b-overlay>
-					</b-dropdown>
-				</div>
-			</template>
-		</b-table>
+							</b-overlay>
+						</b-dropdown>
+					</div>
+				</template>
+			</b-table>
+		</div>
 
 		<TableFooterControls :controls="tableControls" />
 
@@ -136,6 +142,7 @@ export default {
 		breads: [{ title: "Dashboard", link: "/" }, { title: "Purchases" }],
 
 		fields: [
+			{ key: "index", label: "#", class: "text-center d-none d-print-table-cell" },
 			{ key: "date", label: "Date", sortable: true },
 			{ key: "reference", label: "Reference", sortable: true },
 			{ key: "supplier", label: "Supplier", sortable: true },
@@ -145,7 +152,7 @@ export default {
 			{ key: "paid", label: "Paid", sortable: true },
 			{ key: "due", label: "Due", sortable: true },
 			{ key: "paymentStatus", label: "Payment Status", sortable: true },
-			{ key: "actions", label: "Actions", class: "d-print-none" }
+			{ key: "actions", label: "Actions", class: "d-print-none", tdClass: "d-print-none", thClass: "d-print-none" }
 		],
 		filterationFields: { date: "", reference: "", supplier: "", warehouse: "", status: "", paymentStatus: "" },
 		searchIn: { reference: true, date: false }
