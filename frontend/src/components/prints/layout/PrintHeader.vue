98605e93d461b4a39f7c2ee5d49e46e674867af0
Tersawy <mohamedtersawy96@gmail.com>
2022-05-05 16:03:52 +0200

    Added prints components for all prints

---
+++ frontend/src/components/prints/layout/PrintHeader.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="d-flex justify-content-between pb-3 border-bottom" style="border-width: 2px !important">
+		<h1 class="mb-0 d-none d-md-block d-print-block">Locanos</h1>
+		<div class="text-right w-100 text-center text-md-right text-print-right">
+			<h1 class="font-weight-light">Purchase Order</h1>
+			<h2 class="text-muted mb-1">Locanos</h2>
+			<span class="text-muted fs-14">United States</span>
+		</div>
+	</div>
+</template>
