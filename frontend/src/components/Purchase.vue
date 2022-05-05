04bff1f262b862d6dfd2118651b7696b53dfe348
Tersawy <mohamedtersawy96@gmail.com>
2022-05-05 16:05:28 +0200

    Added PrintHeader Component To Purchase

--- frontend/src/components/Purchase.vue
+++ frontend/src/components/Purchase.vue
@@ -4,14 +4,7 @@
 			<b-card>
 				<b-row>
 					<b-col cols="12" class="mb-4">
-						<div class="d-flex justify-content-between pb-3 border-bottom" style="border-width: 2px !important">
-							<h1 class="mb-0 d-none d-md-block d-print-block">Locanos</h1>
-							<div class="text-right w-100 text-center text-md-right text-print-right">
-								<h1 class="font-weight-light">Purchase Order</h1>
-								<h2 class="text-muted mb-1">Locanos</h2>
-								<span class="text-muted fs-14">United States</span>
-							</div>
-						</div>
+						<PrintHeader />
 					</b-col>
 				</b-row>
 				<b-row cols="1" cols-md="2" cols-lg="3" class="order-info">
@@ -160,12 +153,14 @@ const DateStr = () => import("@/components/DateStr");
 
 const InvoiceStatus = () => import("@/components/InvoiceStatus");
 
+const PrintHeader = () => import("@/components/prints/layout/PrintHeader");
+
 export default {
 	props: {
 		invoice: { type: Object, default: () => {} }
 	},
 
-	components: { DateStr, InvoiceStatus },
+	components: { PrintHeader, DateStr, InvoiceStatus },
 
 	data() {
 		return {
