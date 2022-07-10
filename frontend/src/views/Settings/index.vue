5b241b778eb5fae1484cf9294791de88614c5734
Tersawy <mohamedtersawy96@gmail.com>
2022-07-10 08:23:35 +0200

    added settings view page

---
+++ frontend/src/views/Settings/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<main-content :breads="breads"> </main-content>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		breads: [{ title: "Dashboard", link: "/" }, { title: "Settings" }]
+	})
+};
+</script>
